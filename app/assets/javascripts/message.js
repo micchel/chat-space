23e6171f6386457f52854d1287c9dcb25e033052
micchel <msaito2112@gmail.com>
2019-07-07 19:59:59 +0900

    fix data to message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,8 +33,8 @@ $(function() {
       processData: false,
       contentType: false
     })
-    .done(function(data) {
-      var html = buildHTML(data);
+    .done(function(message) {
+      var html = buildHTML(message);
       $('.messages').append(html);
       $('form')[0].reset();
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});

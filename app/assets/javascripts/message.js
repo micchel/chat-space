4d492197c0cbed456565521eabcf9f5849d7f631
micchel <msaito2112@gmail.com>
2019-07-07 19:40:41 +0900

    create-automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
 $(function() {
+
   function buildHTML(message) {
     var image = message.image_url ? `<img src=${message.image_url}>` : ``;
-    var html = `<div class='message'>
+    var html = `<div class='message' data-id='${ message.id }'>
                   <div class='upper-info'>
                     <div class='upper-info__talker'>
                       ${ message.user_name }
@@ -19,6 +20,7 @@ $(function() {
                 </div>`
     return html;
   }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -30,15 +32,46 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
-    }).done(function(data) {
+    })
+    .done(function(data) {
       var html = buildHTML(data);
       $('.messages').append(html);
       $('form')[0].reset();
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
       $('.submit-btn').prop('disabled',false);
-    }).fail(function() {
+    })
+    .fail(function() {
       alert('error');
       $('.submit-btn').prop('disabled',false);
     })
   })
+
+  function reloadMessages() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      if ($('.messages')[0]) {
+        var last_message_id = $('.message:last').data('id');
+      } else {
+        var last_message_id = 0
+      }
+      var groupId = $('.group').data('id');
+      $.ajax({
+        url: '/groups/'+groupId+'/api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+      })
+      .fail(function() {
+          alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
+json.id           @message.id
 json.user_name    @message.user.name
 json.created_time @message.created_at.strftime("%Y/%m/%d(%a) %H:%M:%S")
 json.content      @message.content

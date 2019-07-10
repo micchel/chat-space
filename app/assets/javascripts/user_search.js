7cb34b24e4d16bbf23b3f8c9001bbb3ff147c93f
micchel <msaito2112@gmail.com>
2019-07-10 15:03:02 +0900

    delete console.log

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -9,7 +9,6 @@ $(function() {
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</div>
                 </div>`
-                console.log(html)
     search_list.append(html);
   }
 

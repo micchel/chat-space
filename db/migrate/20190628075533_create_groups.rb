545254461cf39705eb5472492e5e91969a212f16
micchel <msaito2112@gmail.com>
2019-07-10 18:37:11 +0900

    delete add index

--- db/migrate/20190628075533_create_groups.rb
+++ db/migrate/20190628075533_create_groups.rb
@@ -2,8 +2,8 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string       :name, null: false
-      add_index :name, unique: true
       t.timestamps
     end
+    
   end
 end
